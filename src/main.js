b2a0f085c2f9bfb0a68c081b8572654dc1cd707d
Molly <mojonesycreative@gmail.com>
2022-03-16 22:35:51 -0400

    Added new function and test

--- src/main.js
+++ src/main.js
@@ -11,11 +11,27 @@ function findCatById(catBreeds, id) {
 }
 
 
+function getHeightAndWeightByName(catBreeds, name) {
+    let result;
+    for (let i = 0; i < catBreeds.length; i++) {
+        const breedName = catBreeds[i].name;
+        if (breedName !== name) result = 'No such cat.';
+        if (breedName === name) {
+            const height = catBreeds[i].image.height;
+            const weight = catBreeds[i].weight.imperial;
+            result = { height, weight };
+            break;
+        }
+    }
+    return result;
+}
 
 
+findCatById(catBreeds, 'mcoo');
+getHeightAndWeightByName(catBreeds, 'Abyssinian');
 
-findCatById("mcoo", catBreeds);
-
-
-module.exports = findCatById;
+module.exports = {
+    findCatById, 
+    getHeightAndWeightByName,
+};
 
--- test/main.test.js
+++ test/main.test.js
@@ -1,6 +1,9 @@
 const { expect } = require("chai");
 const { catBreeds } = require("../src/cat-breeds");
-const findCatById = require("../src/main");
+const { 
+    findCatById, 
+    getHeightAndWeightByName,
+} = require("../src/main");
 
 describe("findCatById", () => {
     it("should return the cat name associated with the given id", () => {
@@ -9,3 +12,16 @@ describe("findCatById", () => {
         expect(actual).to.equal(expected);
     });
 });
+
+describe ("getHeightAndWeightByName", () => {
+    it ("should return an object containing given cat's height and weight", () => {
+        const actual = getHeightAndWeightByName(catBreeds, "Abyssinian");
+        const expected = { 'height': 1445, 'weight': '7  -  10' };
+        expect(actual).to.eql(expected);
+    });
+
+    it ("should return 'No such cat.' if cat name cannot be found", () => {
+        const actual = getHeightAndWeightByName(catBreeds, "Cattycat");
+        expect(actual).to.equal('No such cat.');
+    })
+})
